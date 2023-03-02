fb32187cd872aaab02ff2906e1b3410f1f564bc1
uhakdt <uhakdt@gmail.com>
2023-03-02 22:55:04 +0000

    Added get request to get response from API + cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [licensePlate, setLicensePlate] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.get(
+        `https://checkmycarapi.uhakdt.repl.co/api/v1/dvla/${licensePlate}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Add License Plate Number</h1>
+        <input
+          type="text"
+          placeholder="License Plate"
+          value={licensePlate}
+          onChange={(e) => setLicensePlate(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
       </header>
     </div>
   );
