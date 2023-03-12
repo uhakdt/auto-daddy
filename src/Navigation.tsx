7c140bc159988a1b2ce6857b1cf26813a2fd7ac9
uhakdt <uhakdt@gmail.com>
2023-03-12 12:25:54 +0000

    Added authentication using firebase and some base pages: login, register, reset and dashboard

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,27 +1,41 @@
 import { Link, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import AboutPage from "./AboutPage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import ResetPage from "./ResetPage";
+import DashboardPage from "./DashboardPage";
 
 function Navigation() {
-	return (
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-				</ul>
-			</nav>
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
 
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/about" element={<AboutPage />} />
-			</Routes>
-		</div>
-	);
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reset" element={<ResetPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Navigation;
