791e172d6f79e92fce92b345ce6a41e0cda4b1df
uhakdt <uhakdt@gmail.com>
2023-06-26 00:58:14 +0100

    Refactored Navigation

---
+++ src/Header.jsx
@@ -0,0 +1,189 @@
+import React, { useState, useContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useHandleLogout } from "./auxiliaryHooks/authHooks";
+import { useNavigate } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Button,
+  Typography,
+  IconButton,
+  SwipeableDrawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { auth } from "./firebase";
+import { AppContext } from "./appContext";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <Link to="/" style={{ textDecoration: "none" }}>
+      <Typography
+        variant="h4"
+        component="div"
+        sx={{ flexGrow: 1, color: "black" }}
+      >
+        Auto Daddy
+      </Typography>
+    </Link>
+  );
+};
+
+function Header() {
+  const { previousPage, setPreviousPage } = useContext(AppContext);
+  const [user, loading] = useAuthState(auth);
+  const handleLogout = useHandleLogout();
+  const navigate = useNavigate();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const list = () => (
+    <Box sx={{ width: 250 }}>
+      <List>
+        {user ? (
+          <>
+            <ListItem
+              onClick={() => {
+                navigate(`/dashboard`);
+                toggleDrawer(false)();
+              }}
+            >
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                handleLogout();
+                toggleDrawer(false)();
+              }}
+            >
+              <ListItemText primary="Log out" />
+            </ListItem>
+          </>
+        ) : (
+          <>
+            <ListItem
+              onClick={() => {
+                navigate(`/auth/login`);
+                toggleDrawer(false)();
+              }}
+            >
+              <ListItemText primary="Login" />
+            </ListItem>
+            <ListItem
+              onClick={() => {
+                navigate(`/auth/register`);
+                toggleDrawer(false)();
+              }}
+            >
+              <ListItemText className="auth__btn" primary="Sign Up" />
+            </ListItem>
+          </>
+        )}
+      </List>
+    </Box>
+  );
+
+  return (
+    <AppBar
+      position="fixed"
+      sx={{ backgroundColor: "#e0e1e9", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Logo />
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2, display: { sm: "none" }, color: "black" }}
+          onClick={toggleDrawer(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <SwipeableDrawer
+          anchor="right"
+          open={drawerOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          {list()}
+        </SwipeableDrawer>
+        <Box
+          sx={{
+            alignItems: "center",
+            justifyContent: "flex-end",
+            paddingRight: "50px",
+            display: { xs: "none", sm: "flex" },
+          }}
+        >
+          {loading ? null : user ? (
+            <>
+              <Button
+                color="inherit"
+                onClick={() => navigate("/dashboard")}
+                sx={{
+                  color: "black",
+                  fontSize: "14px",
+                  mr: "16px",
+                }}
+              >
+                Dashboard
+              </Button>
+              <Button
+                color="inherit"
+                onClick={handleLogout}
+                sx={{
+                  color: "black",
+                  fontSize: "14px",
+                }}
+              >
+                Log out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                color="inherit"
+                onClick={() => {
+                  navigate("/auth/login");
+                  setPreviousPage("/");
+                }}
+                sx={{
+                  color: "black",
+                  fontSize: "14px",
+                  mr: "16px",
+                }}
+              >
+                Login
+              </Button>
+              <button
+                className="auth__btn"
+                onClick={() => {
+                  navigate("/auth/register");
+                  setPreviousPage("/");
+                }}
+              >
+                Sign Up
+              </button>
+            </>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
---
+++ src/Routes.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AuthPage from "./pages/AuthAndAccount/AuthPage";
+import DashboardPage from "./pages/DashboardPage";
+import PackagesPage from "./pages/PackagesPage";
+import PrivacyPage from "./pages/Policy/PrivacyPage";
+import TermsPage from "./pages/Policy/TermsPage";
+import CookiesPage from "./pages/Policy/CookiesPage";
+import DataDeletionPage from "./pages/Policy/DataDeletionPage";
+import GDPRPage from "./pages/Policy/GDPRPage";
+import MoneyBackPage from "./pages/Policy/MoneyBackPage";
+import ContactUsPage from "./pages/ContactUsPage";
+
+function MyRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/packages" element={<PackagesPage />} />
+      <Route path="/auth/*" element={<AuthPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/privacy" element={<PrivacyPage />} />
+      <Route path="/terms" element={<TermsPage />} />
+      <Route path="/cookies" element={<CookiesPage />} />
+      <Route path="/data-deletion" element={<DataDeletionPage />} />
+      <Route path="/gdpr" element={<GDPRPage />} />
+      <Route path="/money-back" element={<MoneyBackPage />} />
+      <Route path="/contact-us" element={<ContactUsPage />} />
+    </Routes>
+  );
+}
+
+export default MyRoutes;
