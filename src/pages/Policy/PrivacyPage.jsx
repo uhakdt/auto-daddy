543370372f5bcea7414dcbfaa6e094e42d3ed83d
uhakdt <uhakdt@gmail.com>
2023-08-19 05:04:44 +0100

    T&C and privacy policy pages info added.

--- src/pages/Policy/PrivacyPage.jsx
+++ src/pages/Policy/PrivacyPage.jsx
@@ -4,11 +4,61 @@ import "./PolicyPageStyles.css";
 const PrivacyPage = () => {
   return (
     <div className="page">
-      <h1>Money Back Guarantee</h1>
-      <p>
-        We offer a 30-day money-back guarantee. If you are not satisfied with
-        our service, you can request a full refund within 30 days of purchase.
+      <h1>Privacy Guidelines</h1>
+
+
+      <h4>1. The Platform</h4>
+
+      <p>Autodaddy.co.uk and its proprietors undertake a proactive stance towards user privacy, and ensure that requisite measures are implemented to safeguard the privacy of its users throughout their browsing journey. Autodaddy.co.uk adheres to all UK domestic laws and statutes concerning user privacy.
       </p>
+
+      <h4>2. Utilisation of Cookies</h4>
+
+      <p>To enhance the user experience, Autodaddy.co.uk deploys cookies during user visits. If relevant, the website utilises a cookie management system, enabling users during their first visit to accept or refuse the usage of cookies on their computer or device. This aligns with recent legislative prerequisites necessitating explicit user consent prior to deploying or reading files such as cookies on a user's device.
+      </p>
+      <p>Cookies are small files stored on the user's computer hard drive, which monitor, save, and archive data regarding the user's engagement with and use of the website. This functionality allows Autodaddy.co.uk to offer users a bespoke experience. Users who wish to refuse the utilisation and storage of cookies by this website on their hard drive should adjust their browser's security settings accordingly to block all cookies from this website and any third-party service providers.
+      </p>
+      <p>Autodaddy.co.uk utilises tracking software, like Google Analytics, to analyse visitor behaviour for a better understanding of user engagement. This software will store a cookie on your hard drive to monitor your interaction and use of the website, without storing, saving or gathering personal information. For more details, you can review Google's privacy policy here: https://policies.google.com/privacy.
+      </p>
+
+      <h4>3. What happens to your data?</h4>
+
+      <p>Your personal information remains confidential and is stored securely until it becomes redundant or has no further use, as per the Data Protection Act 1998.
+      </p>
+      <p>Autodaddy.co.uk and its owners utilise the provided information to offer additional information about their products or services, or to help answer any inquiries or questions you may have. This may include subscribing you to any email newsletter program the website runs, provided you were informed and you granted explicit consent during the form-to-email process.
+      </p>
+      <p>In accordance with UK GDPR, we assure that your personal data will be processed lawfully, fairly, and transparently, without negatively impacting your rights. We will only process your personal data if it aligns with at least one of the following conditions:
+      </p>
+      <ul>
+        <li>a) You have given consent to the processing of your personal data for one or more specific purposes.</li>
+        <li>b) Processing is necessary for the execution of a contract to which you are a party or for steps taken at your request prior to entering into a contract.</li>
+        <li>c) Processing is necessary for compliance with a legal obligation that we must fulfil.</li>
+        <li>d) Processing is necessary to protect the vital interests of you or of another natural person.</li>
+        <li>e) Processing is necessary for a task carried out in the public interest or in the exercise of an official authority vested in us.</li>
+        <li>f) Processing is necessary for the purposes of the legitimate interests pursued by us or by a third party unless such interests are overridden by your rights and freedoms.</li>
+      </ul>
+      <p>If you supply us with the personal data of another data subject, it is your responsibility to ensure you have their consent for us to process that data as per this Privacy Policy, and to make them aware of this Privacy Policy.
+      </p>
+
+      <h4>Disclosure of your Information</h4>
+      <p>We may need to disclose your Information under certain circumstances:
+      </p>
+      <ul>
+        <li>Strategic Business Transactions: In the event of a potential sale, merger, or acquisition of our company or assets, your personal data might be disclosed to the prospective buyer or entity who will also be subject to autodaddy.co.uk privacy guidelines.</li>
+        <li>Legal & Safety Obligations: We reserve the right to disclose your information if it's mandated by law, or if such action is deemed necessary to safeguard other individuals' rights, property, or safety.</li>
+        <li>Fraud Prevention & Credit Risks: We may share certain information with other businesses to mitigate risks associated with fraud and credit-related threats.</li>
+        <li>Third-Party Service Providers: We occasionally collaborate with third-party entities to enhance our service offerings. This might entail sharing your data with them for purposes like payment processing, search optimisation, marketing, or advertising.</li>
+        <li>Handling of Your Data with Third Parties: If any third party requires access to your information to deliver the aforementioned services, rest assured that we take every precaution to ensure the safe and GDPR-compliant handling of your data. Such third parties are strictly prohibited from utilising your data beyond the confines of their contractual obligations with us, ensuring that your privacy remains uncompromised.</li>
+      </ul>
+
+      <h4>Social Media Engagement</h4>
+      <p>The interaction, engagement, and actions made via external social media platforms in which Autodaddy.co.uk and its owners participate are subject to the respective platform's terms and conditions and privacy policies.
+      </p>
+      <p>Users are advised to use social media platforms judiciously and to exercise care regarding their privacy and personal details. Autodaddy.co.uk or its owners will never request sensitive information via social media platforms and encourage users wishing to discuss sensitive information to contact them through primary channels like telephone or email.
+      </p>
+      <p>Autodaddy.co.uk may use social sharing buttons which facilitate the sharing of web content directly from our web pages to the concerned social media platform. Users are advised to use these social sharing buttons at their discretion, bearing in mind that the social media platform may monitor and store your request to share a webpage via your account on their platform.
+      </p>
+      
     </div>
   );
 };
--- src/pages/Policy/TermsPage.jsx
+++ src/pages/Policy/TermsPage.jsx
@@ -5,14 +5,119 @@ const TermsPage = () => {
   return (
     <div className="page">
       <h1>Terms & Conditions</h1>
-      <p>
-        Welcome to our website. If you continue to browse and use this website,
-        you are agreeing to comply with and be bound by the following terms and
-        conditions of use, which together with our privacy policy govern
-        [business name]'s relationship with you in relation to this website. If
-        you disagree with any part of these terms and conditions, please do not
-        use our website.
-      </p>
+      <h4>Definitions:</h4>
+      <ol>
+        <li>“We”, “Us”, and “Our” pertains solely to owners of autodaddy.co.uk</li>
+        <li>“You” “Your” pertains to the user.</li>
+        <li>"Contract" means the contract between you and us for the supply of the Information made subject to these Terms.</li>
+        <li>"Information" means the information supplied by us relating to a motor vehicle recorded on our electronic database in response to your Enquiry.</li>
+      </ol>
+
+      <h4>General Provisions
+      </h4>
+      <ol>
+        <li>Your use of this service indicates your agreement to adhere to these Terms.</li>
+        <li>If you disagree with these Terms, you are advised not to use our service.</li>
+        <li>We offer no assurances regarding the accuracy of our data to any Motor Trade representatives.</li>
+        <li>You also consent to not employing automated tools or systems to scrape data from our service other than obtaining our reports.</li>
+        <li>Using our Service:</li>
+        <ul>
+          <li>The agreement to provide vehicle data check forms a contract solely between Us and You, intended exclusively for your personal use.</li>
+          <li>You are not entitled to cancel the contract, and the Consumer Protection (Distance Selling) Regulations 2000 are not applicable.</li>
+          <li>Sharing this information with any third party is prohibited.</li>
+          <li>Our data does not comment on the vehicle's roadworthiness or overall condition.</li>
+          <li>The entirety of the agreement between You and Us is contained in these Terms.</li>
+        </ul>
+      </ol>
+      <h4>Main Contract Terms</h4>
+
+      <ol>
+        <li>We endeavour to maintain accuracy in the data provided to You, yet, we cannot provide an absolute guarantee of its correctness. We deliver this data "as is," promising only that it is a representative copy of the information provided to Us.</li>
+        <li>We undertake reasonable measures to keep our data current. You acknowledge that there may be some delay while we process new data from sources such as DVLA, insurance companies, the police, and others, before making it available to You.</li>
+        <li>We have no authority over the timelines or quality of data provided by our third-party suppliers. We will share with you the most recent data in our possession.</li>
+        <li>Data Accuracy and Usage</li>
+        <ul>
+          <li>We assure you that our data accurately reflects the information we obtain from our third-party sources. This does not entail any liability on our part for any inaccuracies in the data held by these third parties. Our liability excludes any mistakes in that data.</li>
+          <li>You agree that the data we supply should be used alongside accepted good practices when buying a motor vehicle and should not be relied upon solely.</li>
+          <li>Buying a motor vehicle carries risks. Our service is one measure to guard against misrepresentations or fraudulent practices. However, our data cannot detect "ringed" or "cloned" vehicles. These vehicles bear incorrect number plates and/or VIN numbers. If a vehicle with incorrect number plates was purchased based on data from us, we offer no compensation or warranty.</li>
+          <li>We may alert you if the vehicle's registration number, provided by You, is marked as stolen by the police.</li>
+          <li>We do not guarantee that the content is free from viruses, or that you will always be able to use the website without any interruption or error. If you download any content to your computer or take the content from the website in any other way, you do so at your own risk. We will not be responsible for any damage to your computer or any of the information on it.</li>
+          <li>These Terms do not infringe upon your statutory consumer rights.</li>
+          <li>We reserve the right to share your details with the police if a vehicle report request concerns a vehicle flagged as stolen, and You consent to this provision.</li>
+          <li>However, nothing in these terms removes or limits our liability for death or personal injury caused by our negligence or for any liability which we cannot limit or exclude under the relevant law.</li>
+        </ul>
+      </ol>
+      <h4>AI-Generated Content and Disclaimers</h4>
+      <ol>
+        <li>Nature of AI-Generated Content: AutoDaddy utilises sophisticated artificial intelligence (AI) technology, developed by OpenAI, to create summaries and analyses of vehicle history data obtained from third-party sources. Please understand that this AI technology does not independently authenticate, cross-verify, or corroborate the data it processes.</li>
+        <li>Limitation of Liability: While AutoDaddy endeavours to provide accurate and useful insights, AutoDaddy, along with its affiliates, directors, officers, employees, and agents, cannot be held liable for the accuracy, utility, or implications of the AI-generated insights. Decisions made based on these insights are entirely the responsibility of the user. We strongly advise users to seek professional guidance or conduct their own independent research before making decisions based on the provided information.</li>
+        <li>Disclaimers: Autodaddy makes no guarantees, whether express or implied, in relation to the AI-generated insights. This includes, but is not limited to, any implied warranties of quality, fitness for a particular purpose, or non-infringement. Autodaddy does not warrant that the AI-generated insights will be free from errors, inaccuracies, or omissions, nor does it guarantee the completeness or applicability of the information to your particular circumstances.</li>
+        <li>Indemnity: The user agrees to indemnify, defend, and hold harmless AutoDaddy, its affiliates, directors, officers, employees, and agents from and against all losses, expenses, damages, and costs, including reasonable legal fees, arising out of or in connection with any use or reliance on the AI-generated insights.</li>
+        <li>Acknowledgement: By using our services, you acknowledge and agree that you understand the potential limitations and risks associated with AI-generated insights, and you agree not to hold Autodaddy liable for any inaccuracies, misunderstandings, or decisions made based on these insights.</li>
+      </ol>
+      <h4>Our Intellectual Property Rights</h4>
+      <ol>
+        <li>We own or hold licenses for all the names, product names, titles, software, routines, and copyrights we use to deliver our service. Unauthorised use, copying, reproduction, redistribution, display, alteration, creation of derivative works from, or extraction or re-use of any material accessed through our service is strictly prohibited, except where expressly authorised by Us. Such unauthorised use may be unlawful.</li>
+      </ol>
+      <h4>Territorial Application</h4>
+      <ol>
+        <li>Our service is intended for residents of England, Scotland, and Wales (the "Territory"). If you are not a resident of the Territory, you may not use our service.</li>
+        <li>By using our service, you affirm your residency is in compliance with our Terms.</li>
+      </ol>
+
+      <h4>Governing Law</h4>
+      <ol>
+        <li>Your use of our service and these Terms are governed and construed by English law.</li>
+        <li>You agree to the jurisdiction of English courts for any disputes between You and Us.</li>
+      </ol>
+
+      <h4>Refund Policy</h4>
+      <ol>
+        <li>Our vehicle check constitutes a digital download. The relevant details are laid out in section 15.</li>
+      </ol>
+
+      <h4>Consumer Rights</h4>
+      <ol>
+        <li>As per the Consumer Rights Act, digital content is data produced and supplied in digital form. Therefore, anything you download or stream, including apps, ebooks, films, games, or music, must be:</li>
+        <ul>
+          <li>Of satisfactory quality</li>
+          <li>Fit for a particular purpose</li>
+          <li>As described by the seller</li>
+        </ul>
+        <li>If your digital content is faulty and fails to meet the above criteria, you have the right to have your digital product repaired or replaced.</li>
+      </ol>
+
+      <h4>Charges</h4>
+      <ol>
+        <li>You shall render payment for the auto check service at our prevailing rates, which may be adjusted periodically.</li>
+        <li>Acceptable payment methods, such as credit cards or others, will be detailed on our Website.</li>
+        <li>Provision of our information is contingent on the prior fulfilment of payment.</li>
+      </ol>
+
+      <h4>Your Right to Cancel</h4>
+      <ol>
+        <li>Under the Consumer Protection Distance Selling Regulations 2000, your right to cancel the service expires upon our provision of the Information, provided that this action is performed with your explicit agreement and before the conclusion of the cancellation period.</li>
+      </ol>
+      <h4>Termination</h4>
+      <ol>
+        <li>We reserve an unfettered right to suspend or terminate your access to our service at any given time, without notice or liability, for any reason, particularly in the event of any violation of these Terms. Such termination implies an immediate cessation of your service usage rights.</li>
+        <li>In the event of termination, the provisions of these Terms that naturally extend beyond termination will endure. These enduring provisions include, but are not limited to, ownership provisions, disclaimers of warranties, indemnification clauses, and limitations of liability.</li>
+      </ol>
+
+      <h4>Changes</h4>
+      <ol>
+        <li>We hold an unreserved right to modify or replace these Terms at our discretion.</li>
+        <li>Should there be a significant amendment, we will strive, where possible, to provide a minimum of 30 days' notice before the amended Terms come into effect. The determination of what qualifies as a significant amendment rests solely with us.</li>
+        <li>By accessing or using our service following the implementation of such revisions, you consent to abide by the updated terms. If you disagree with the new terms, you are obliged to discontinue your use of the service.</li>
+      </ol>
+
+      <h4>Cancelling a Digital Download</h4>
+      <ol>
+        <li>Digital downloads have a unique classification under the Consumer Contracts Regulations and are neither services nor goods.</li>
+        <li>To download something within 14 days of purchase, you must give your consent to waive the 14-day cooling-off period. Without your consent, you may not download your digital content until this period has ended to prevent you from changing your mind post-download.</li>
+        <li>By selecting the 'buy' button, you consent to waiving the 14-day cooling-off period, enabling immediate receipt of your vehicle check. If you prefer to retain the 14-day cooling-off period, please contact our support team to discuss your needs. We operate exclusively as a digital download company.</li>
+      </ol>
+
     </div>
   );
 };
