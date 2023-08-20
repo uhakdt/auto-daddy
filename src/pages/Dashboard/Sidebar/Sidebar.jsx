f3385e84e022da14e65b37c282d7fd5fd590f74e
Baban Rashid <baban@healthprofessionals.co.uk>
2023-08-20 16:54:01 +0100

    sidebar left gradient line design changes

--- src/pages/Dashboard/Sidebar/Sidebar.jsx
+++ src/pages/Dashboard/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AppContext } from "../../../appContext";
 
 import { CapitalizeEachWord } from "../../../auxiliaryFunctions/stringFunctions";
@@ -16,6 +16,41 @@ function Sidebar({
   selectedOrderId,
   setSelectedOrderId,
 }) {
+
+  //sidebar gradient line
+  useEffect(() => {
+    const handleScroll = () => {
+      let scrollPercent = (window.scrollY) / (document.documentElement.scrollHeight - window.innerHeight);
+      let colorChange = Math.min(1, scrollPercent * 2);
+
+      let newColor = `linear-gradient(to bottom, 
+        rgba(${hexToRgb('--dark-purple').r}, ${hexToRgb('--dark-purple').g}, ${hexToRgb('--dark-purple').b}, ${1 - colorChange}), 
+        rgba(${hexToRgb('--orange').r}, ${hexToRgb('--orange').g}, ${hexToRgb('--orange').b}, ${colorChange}))`;
+
+      document.body.style.setProperty("--scroll-gradient", newColor);
+    }
+
+    const hexToRgb = (hexColorVar) => {
+      let hex = getComputedStyle(document.documentElement)
+        .getPropertyValue(hexColorVar)
+        .trim();
+      hex = hex.replace(/^#/, '');
+
+      let bigint = parseInt(hex, 16);
+      let r = (bigint >> 16) & 255;
+      let g = (bigint >> 8) & 255;
+      let b = bigint & 255;
+
+      return { r: r, g: g, b: b };
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
   const { setCurrentOrder } = useContext(AppContext);
   const [selectedOrder, setSelectedOrder] = useState(null);
   const handleOrderClick = (order) => {
@@ -24,14 +59,18 @@ function Sidebar({
   };
   const iconsUrl = process.env.PUBLIC_URL + "/icons/";
 
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
+  useEffect(() => {
+    const isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
 
-  if (isMobile) {
-    setIsSidebarOpen(false);
-  }
+    if (isMobile) {
+      setIsSidebarOpen(false);
+    }
+  }, []); //
+
+
 
   return (
     <Box className="sidebar-box">
@@ -59,11 +98,10 @@ function Sidebar({
                     setCurrentOrder(order);
                     setSelectedOrderId(order.orderId); // Update the selected order ID
                   }}
-                  className={`order-button-container ${
-                    selectedOrderId === order.orderId
-                      ? "order-button-container-selected"
-                      : ""
-                  }`}
+                  className={`order-button-container ${selectedOrderId === order.orderId
+                    ? "order-button-container-selected"
+                    : ""
+                    }`}
                 >
                   <div>
                     <img alt="Google" src={iconsUrl + "car.svg"} height={20} />{" "}
